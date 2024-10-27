64b32a2712ef055fd732d04e48a9b39be60e38fc
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-27 14:30:51 -0300

    creating new function

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,31 @@ import { useState } from "react";
 export default () => {
     const [start, setStart] = useState(false);
     const [disabledGame, setDisabledGame] = useState(false);
+    const [winner, setWinner] = useState("");
 
     const [playerPoints, setPlayerPoints] = useState({ player: 0, bot: 0 });
 
     const getRandomCard = () => {
         return Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    };
+
+    const getWinner = () => {
+        if (playerPoints.player > 21) {
+            setWinner("Bot ganhou");
+        } else if (playerPoints.bot > 21) {
+            setWinner("Player ganhou");
+        } else if (playerPoints.bot > playerPoints.player) {
+            setWinner("Bot ganhou");
+        } else if (playerPoints.bot < playerPoints.player) {
+            setWinner("Player ganhou");
+        } else {
+            setWinner("empate");
+        }
+    };
+
+    const finishGame = () => {
+        setDisabledGame(true);
+        getWinner();
     };
 
     return (
@@ -41,12 +61,10 @@ export default () => {
                     ) : (
                         <button disabled>take</button>
                     )}
-                    <button
-                        className="dark-btn"
-                        onClick={() => setDisabledGame(true)}
-                    >
+                    <button className="dark-btn" onClick={finishGame}>
                         finish
                     </button>
+                    <p>{winner}</p>
                 </main>
             )}
         </>
